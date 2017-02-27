507828cf345827d447d6ec5870f2a7a41124f7e1
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-02-26 20:30:25 -0500

    Now it's done

--- assignmenttwo/src/h2/SquareRoot.java
+++ assignmenttwo/src/h2/SquareRoot.java
@@ -5,18 +5,17 @@ public class SquareRoot {
 	double x;
 	double error;
 	double guess;
-	double y = x - error;
-	public static double SquareRoot(double x, double error, double guess){
+	public static double squareRoot(double x, double error, double guess){
 	
 	if((guess*guess)-x < error){
 		return guess;
 	}
 		guess = ((guess + (x / guess))*0.5);
-		return SquareRoot(x, error, guess);
+		return squareRoot(x, error, guess);
 	
 	}
 	public static void main (String[] args){
-		System.out.println(SquareRoot (16, 0.00000000000001, 5));
+		System.out.println(squareRoot (16, 0.000000001, 5));
 		
 	}
 
