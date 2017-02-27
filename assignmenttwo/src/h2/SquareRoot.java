a7203e85888ff8e379451f4a0c47a077a48b40df
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-02-26 20:13:37 -0500

    Come on

--- assignmenttwo/src/h2/SquareRoot.java
+++ assignmenttwo/src/h2/SquareRoot.java
@@ -2,4 +2,23 @@ package h2;
 
 public class SquareRoot {
 
+	double x;
+	double error;
+	double guess;
+	double y = x - error;
+	public static double SquareRoot(double x, double error, double guess){
+	
+	if((guess*guess)-x < error){
+		return guess;
+	}else{
+		guess = ((guess + (x / guess))*0.5);
+		SquareRoot(x, error, guess);
+		return guess;
+	}
+	}
+	public static void main (String[] args){
+		System.out.println(SquareRoot (16.0, 0.0000000000000001, 4.1));
+		
+	}
+
 }
